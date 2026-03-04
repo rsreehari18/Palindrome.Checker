b715357dfeb6aa2d0ef3b67b79024e609e945c6b
rsreehari18 <rsreeharikumar@gmail.com>
2026-03-05 00:30:29 +0530

    Created UC13

---
+++ UseCase13PalindromeCheckerApp.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class UseCase13PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input: ");
+        String input = sc.nextLine();
+
+        long startTime = System.nanoTime();
+
+        boolean result = isPalindrome(input);
+
+        long endTime = System.nanoTime();
+
+        long executionTime = endTime - startTime;
+
+        System.out.println("Is Palindrome?: " + result);
+        System.out.println("Execution Time: " + executionTime + " ns");
+
+        sc.close();
+    }
+
+    public static boolean isPalindrome(String str) {
+
+        int start = 0;
+        int end = str.length() - 1;
+
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}
