fe720c451b30dc1f735adb73db93bbf507c8c8ad
rsreehari18 <rsreeharikumar@gmail.com>
2026-03-05 00:26:43 +0530

    Created UC11

---
+++ UseCase11PalindromeCheckerApp.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class UseCase11PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input: ");
+        String input = sc.nextLine();
+
+        PalindromeService service = new PalindromeService();
+
+        boolean result = service.checkPalindrome(input);
+
+        System.out.println("Is Palindrome?: " + result);
+
+        sc.close();
+    }
+}
+
+// Service class containing palindrome logic
+class PalindromeService {
+
+    public boolean checkPalindrome(String input) {
+
+        int start = 0;
+        int end = input.length() - 1;
+
+        while (start < end) {
+            if (input.charAt(start) != input.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}
