7877d4cfa3b49bf6b606eeae98f3c4908a9ec46a
rsreehari18 <rsreeharikumar@gmail.com>
2026-03-05 00:23:33 +0530

    Created UC9

---
+++ UseCase9PalindromeCheckerApp.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class UseCase9PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input: ");
+        String input = sc.nextLine();
+
+        boolean result = check(input, 0, input.length() - 1);
+
+        System.out.println("Is Palindrome?: " + result);
+
+        sc.close();
+    }
+
+    // Recursive function
+    private static boolean check(String s, int start, int end) {
+
+        // Base condition
+        if (start >= end) {
+            return true;
+        }
+
+        // If characters do not match
+        if (s.charAt(start) != s.charAt(end)) {
+            return false;
+        }
+
+        // Recursive call
+        return check(s, start + 1, end - 1);
+    }
+}
